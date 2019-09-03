83a951656d9c880d4e560f4ca4b02166d0903a51
gabriels17 <gabriel.sighvatsson@gmail.com>
2019-09-03 16:03:13 +0000

    Added props to Dto's

--- TechnicalRadiation.Models/DTOs/AuthorDetailDto.cs
+++ TechnicalRadiation.Models/DTOs/AuthorDetailDto.cs
@@ -2,6 +2,9 @@ namespace TechnicalRadiation.Models.DTOs
 {
     public class AuthorDetailDto
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProfileImgSource { get; set; }
+        public string Bio { get; set; }
     }
 }
