e4c3d4aef79a054113a51e6976e7c8bffd6fdda4
nannalop <natalia17@ru.is>
2019-09-04 16:01:29 +0000

    getAllAuthors() implemented

---
+++ TechnicalRadiation.WebApi/Controllers/AuthorController.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using TechnicalRadiation.Models.Entities;
+using TechnicalRadiation.Models;
+using TechnicalRadiation.Models.DTOs;
+using TechnicalRadiation.Services;
+using TechnicalRadiation.Models.InputModels;
+using AutoMapper;
+
+namespace TechnicalRadiation.WebApi.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private AuthorService _authorService;
+
+        public AuthorController(IMapper mapper)
+        {
+            _authorService = new AuthorService(mapper);
+        }
+
+        // GET /api/authors
+        [HttpGet]
+        [Route("{}")]
+        public IActionResult GetAllAuthors()
+        {
+            var authors = _authorService.GetAllAuthors();
+            return Ok(authors);
+        }
+/*
+        // GET /api/5
+        [HttpGet]
+        [Route("{id:int}", Name = "GetNewsById")]
+        public ActionResult<string> GetNewsById(int id)
+        {
+            var news = _newsService.GetNewsById(id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+            return Ok(news);
+        }
+
+        // POST /api
+        [Route("")]
+        [HttpPost]
+        public IActionResult CreateNews([FromBody] NewsItemInputModel news)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Model is not properly formatted.");
+            }
+            var entity = _newsService.CreateNews(news);
+            return CreatedAtAction("GetNewsById", new { id = entity.Id }, null);
+        }
+
+        // PUT api/news/5
+        [Route("{id:int}")]
+        [HttpPut]
+        public IActionResult UpdateNewsById([FromBody] NewsItemInputModel news, int id)
+        {
+            if (!ModelState.IsValid) { return BadRequest("Model is not properly formatted."); }
+            _newsService.UpdateNewsById(news, id);
+            return NoContent();
+        }
+
+        // DELETE api/news/5
+        [Route("{id:int}")]
+        [HttpDelete]
+        public IActionResult DeleteNewsById(int id)
+        {
+            _newsService.DeleteNewsById(id);
+            return NoContent();
+        }*/
+    }
+}
+
