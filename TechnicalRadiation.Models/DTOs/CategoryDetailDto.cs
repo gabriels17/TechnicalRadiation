6fb2c8acf1b0f1473e279ea536f4b19412af6c2a
gabriels17 <gabriel.sighvatsson@gmail.com>
2019-09-10 16:43:19 +0000

    Setup HyperMediaModel inheritance

--- TechnicalRadiation.Models/DTOs/CategoryDetailDto.cs
+++ TechnicalRadiation.Models/DTOs/CategoryDetailDto.cs
@@ -1,6 +1,6 @@
 namespace TechnicalRadiation.Models.DTOs
 {
-    public class CategoryDetailDto
+    public class CategoryDetailDto : HyperMediaModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
