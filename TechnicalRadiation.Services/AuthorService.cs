6317b206296d2c7ab96770d10384a3e9232d73a7
Arna Jónasardóttir <arnaj17@ru.is>
2019-09-13 14:16:49 +0000

    links added to authorservice

--- TechnicalRadiation.Services/AuthorService.cs
+++ TechnicalRadiation.Services/AuthorService.cs
@@ -13,10 +13,12 @@ namespace TechnicalRadiation.Services
     public class AuthorService
     {
         private AuthorRepo _authorRepo;
+        private NewsRepo _newsRepo;
 
         public AuthorService(IMapper mapper)
         {
             _authorRepo = new AuthorRepo(mapper);
+            _newsRepo = new NewsRepo(mapper);
         }
 
         private AuthorDto AddAuthorDtoLinks(AuthorDto author)
@@ -62,12 +64,35 @@ namespace TechnicalRadiation.Services
 
         public void LinkNewsItemToAuthorById(int aid, int nid)
         {
+            if (aid < 1) { throw new ArgumentOutOfRangeException("Id should not be lower than 1"); }
+            var author = _authorRepo.GetAuthorById(aid);
+            if (author == null) { throw new ResourceNotFoundException($"Category with id {aid} was not found"); }
+            
+            if (nid < 1) { throw new ArgumentOutOfRangeException("Id should not be lower than 1"); }
+            var newsItem = _newsRepo.GetNewsById(nid);
+            if (newsItem == null) { throw new ResourceNotFoundException($"Category with id {nid} was not found"); }
+            
+
             _authorRepo.LinkNewsItemToAuthorById(aid, nid);
         }
 
         public List<NewsItemDto> GetNewsItemsByAuthor(int id)
         {
+            if (id < 1) { throw new ArgumentOutOfRangeException("Id should not be lower than 1"); }
+            var author = _authorRepo.GetAuthorById(id);
+            if (author == null) { throw new ResourceNotFoundException($"Category with id {id} was not found"); }
+
             var newsItems = _authorRepo.GetNewsItemsByAuthor(id);
+            foreach (var n in newsItems)
+            {
+                n.Links.AddReference("edit", $"api/{n.Id}");
+                n.Links.AddReference("self", $"api/{n.Id}");
+                n.Links.AddReference("delete", $"api/{n.Id}");
+                n.Links.AddListReference("authors", _newsRepo.GetAuthorsByNewsItemId(n.Id)
+                    .Select(a => new { href = $"api/authors/{id}"}));
+                n.Links.AddListReference("categories", _newsRepo.GetCategoriesByNewsItemId(n.Id)
+                    .Select(c => new { href = $"api/categories/{c.Id}"}));
+            }
             return newsItems;
         }
 
